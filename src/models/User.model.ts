9d4caea5e21edcb999b9ce016020c9e4cded297b
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2025-12-26 22:18:32 +0530

    refactor(model): update user model fields and fixes

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,22 +1,14 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model, Document  } from 'mongoose';
 
-interface IUser extends Document {
-    firstName: string;
-    lastName: string;
+interface IUser extends Document  {
+    name: string;
     email: string;
     password: string;
-    createdAt: Date;
-    updatedAt: Date;
 }
 
 const userSchema = new Schema<IUser>(
     {
-        firstName: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        lastName: {
+        name: {
             type: String,
             required: true,
             trim: true,
@@ -26,7 +18,10 @@ const userSchema = new Schema<IUser>(
             required: true,
             unique: true,
             lowercase: true,
-            match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                'Please enter a valid email address',
+            ],
         },
         password: {
             type: String,
@@ -39,6 +34,4 @@ const userSchema = new Schema<IUser>(
     }
 );
 
-const User = model<IUser>('User', userSchema);
-
-export default User;
+ export const User = model<IUser>('User', userSchema);
