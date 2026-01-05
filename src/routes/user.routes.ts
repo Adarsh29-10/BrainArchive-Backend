6dd5942270ae6d554ab41dbc8b007659ed3577bf
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-05 12:46:12 +0530

    feat(user.routes): add TypeScript types for request and response in '/me' route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { User } from "../models/User.model";
+import { Request, Response } from 'express';
 
 const router = express.Router()
 
-router.get('/me', (req, res)=>{
+router.get('/me', (req:Request, res:Response)=>{
     res.status(200).json(req.user);
 })
 
