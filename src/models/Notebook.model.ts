cc496dab77944263a14a1ee35c00fd521cc2317f
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-09 22:52:17 +0530

    feat(notebook.model): enhance INotebook and IBlock interfaces with blocks schema

--- src/models/Notebook.model.ts
+++ src/models/Notebook.model.ts
@@ -1,11 +1,43 @@
 import { Schema, model, Document, Types } from 'mongoose'
+import { BlockType } from '../types/blocks.types';
 
+interface IBlock {
+    type: BlockType;
+    content: string;
+    order: number
+}
 interface INotebook extends Document{
     userId: Types.ObjectId;
     title: string;
     description?: string;
+    blocks: Types.DocumentArray<IBlock>;
+    isPublic: boolean;
+    lastActivityAt: Date | null;
     totalTimeSpent: number;
 }
+
+const blockSchema = new Schema<IBlock>(
+    { 
+        type: {
+            type: String,
+            required: true,
+            enum: ['heading', 'paragraph', 'document', 'image', 'video', 'code', 'link', 'bullet' ]
+        },
+        content: {
+            type: String,
+            required: true,
+            trim: true,
+            default: "",
+        },
+        order: {
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        _id: true
+    }
+);
 
 const notebookSchema = new Schema<INotebook>(
     {
@@ -13,6 +45,7 @@ const notebookSchema = new Schema<INotebook>(
             type: Schema.Types.ObjectId,
             ref: 'User',
             required: true,
+            index: true,
         },
         title: {
             type: String,
@@ -24,6 +57,18 @@ const notebookSchema = new Schema<INotebook>(
             type: String,
             trim: true,
             maxlength: 500,
+        },
+        blocks: {
+            type: [blockSchema],
+            default: []
+        },
+        isPublic: {
+            type: Boolean,
+            default: false,
+        },
+        lastActivityAt: {
+            type: Date,
+            default: null,
         },
         totalTimeSpent: {
             type: Number,
