402d2cf1f19a65a05f28264819c628924bacdcb9
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-17 22:51:44 +0530

    fix(auth.middleware): correct user authentication property access for consistency

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,10 +3,10 @@ import { Request, Response, NextFunction } from "express";
 export const attachUser = async (req:Request, _res:Response, next:NextFunction) => {
   const auth = req.auth;
 
-  if (!auth?.user?.sub) return next();
+  if (!auth?.sub) return next();
 
   req.user = {
-    auth0Id: auth.user.sub
+    auth0Id: auth.sub
   };
 
   next();
