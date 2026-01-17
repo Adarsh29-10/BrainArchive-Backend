8db879b43c99d2a7445c1d777d78d33f8fce917d
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-17 22:15:03 +0530

    refactor(auth.middleware): simplify user attachment logic by removing unused code and error handling

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,37 +1,13 @@
-import { User } from "../models/User.model";
 import { Request, Response, NextFunction } from "express";
 
 export const attachUser = async (req:Request, _res:Response, next:NextFunction) => {
-  try {
-    const auth = req.auth;
+  const auth = req.auth;
 
-    if (!auth?.sub) {
-      return next();
-    }
+  if (!auth?.user?.sub) return next();
 
-    const auth0Id = auth.sub;
+  req.user = {
+    auth0Id: auth.user.sub
+  };
 
-    const email =
-      auth.email ||
-      auth["https://brainarchive/email"] ||
-      `${auth0Id}@auth0.local`;
-
-
-    let user = await User.findOne({ auth0Id });
-
-    if (!user) {
-      user = await User.create({
-        auth0Id,
-        email,
-        name: auth.name || "Unknown",
-      });
-
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    console.error("ATTACH USER ERROR:", error);
-    next(error);
-  }
+  next();
 };
