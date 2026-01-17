dd16f534ff150026f2e346f2051f6201067c5aa1
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-17 22:16:29 +0530

    feat(getCurrentUser): implement function to retrieve current user based on auth0Id

---
+++ src/utils/getCurrentUser.ts
@@ -0,0 +1,17 @@
+import { Request } from "express";
+import { User } from "../models/User.model";
+import { ApiError } from "./ApiError";
+
+export const getCurrentUser = async (req: Request) => {
+  if (!req.user?.auth0Id) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const user = await User.findOne({ auth0Id: req.user.auth0Id });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  return user;
+};
