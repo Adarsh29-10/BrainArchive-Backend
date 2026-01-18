530fb5a09eeda13d87e25352d1755f3dbb39eef3
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-01-18 20:52:17 +0530

    refactor(blocks.types): convert BlockType to a constant array for improved type safety

--- src/types/blocks.types.ts
+++ src/types/blocks.types.ts
@@ -1,11 +1,14 @@
-export type BlockType =
-  | "heading"
-  | "heading1"
-  | "heading2"
-  | "paragraph"
-  | "document"
-  | "image"
-  | "video"
-  | "code"
-  | "link"
-  | "bullet";
+export const BlockType = [
+  "heading",
+  "heading1",
+  "heading2",
+  "paragraph",
+  "document",
+  "image",
+  "video",
+  "code",
+  "link",
+  "bullet",
+] as const;
+
+export type BlockType = (typeof BlockType)[number];
