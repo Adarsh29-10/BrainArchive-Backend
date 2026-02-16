adb0390b586f0579e84590578186c5ab811ee258
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-02-17 00:12:30 +0530

    fix(notebook.routes): add addNotebookBlock route to add block

--- src/routes/notebook.routes.ts
+++ src/routes/notebook.routes.ts
@@ -5,7 +5,8 @@ import {
     getNotebookById, 
     updateNotebook, 
     deleteNotebook,
-    updateNotebookBlock
+    updateNotebookBlock,
+    addNotebookBlock
 } from '../controllers/notebook.controller'
 
 const router = express.Router()
@@ -17,6 +18,7 @@ router.get('/:notebookId', getNotebookById);
 router.put('/:notebookId', updateNotebook);
 router.delete('/:notebookId', deleteNotebook);
 
+router.post('/:notebookId/block', addNotebookBlock)
 router.patch('/:notebookId/blocks', updateNotebookBlock)
 
 export default router;
