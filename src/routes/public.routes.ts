1c01d94c3ea8bcff629c59cb27975730c8851b92
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2026-02-18 17:33:40 +0530

    refactor: remove unused controllers, services, and routes for notebooks, sessions, and users

--- src/routes/public.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getPublicNotebooks } from '../controllers/notebook.controller';
-
-const router = express.Router();
-
-router.get('/notebooks', getPublicNotebooks);
-
-export default router;
--- src/routes/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { userMe } from '../controllers/user.controller';
-
-const router = express.Router()
-
-router.post('/me', userMe)
-
-export default router;
